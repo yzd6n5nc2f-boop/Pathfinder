8ccc6acb76fa9d197595dc70ab16a1e3fd404800
Mauricio <yzd6n5nc2f@privaterelay.appleid.com>
2026-01-10 19:53:32 +0000

    Update footer company name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t border-line bg-white px-4 py-4 text-center text-xs text-muted">
-      Innerweb Ventures Limited
+      Innoweb Ventures Ltd
     </footer>
   );
 };
